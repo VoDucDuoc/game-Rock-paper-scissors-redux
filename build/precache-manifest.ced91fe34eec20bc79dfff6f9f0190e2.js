9426f5d5be67cd071d72690d80a3085d32b3b693
unknown <utcungg@gmail.com>
2020-10-16 14:27:17 +0700

    upload

---
+++ build/precache-manifest.ced91fe34eec20bc79dfff6f9f0190e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e02fb2a2dad7e034810df3e98d617b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f8fdf59a35b46b68c84",
+    "url": "/static/css/main.6cde0add.chunk.css"
+  },
+  {
+    "revision": "aa3c298451a4651f0c12",
+    "url": "/static/js/2.aaa6e624.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aaa6e624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8fdf59a35b46b68c84",
+    "url": "/static/js/main.25da6a7b.chunk.js"
+  },
+  {
+    "revision": "763fcd6af76f9270a8b1",
+    "url": "/static/js/runtime-main.2af67aa7.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/static/media/Pony.1716f3f8.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import rootReducer from "./reducer/rootReducer";
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      // window.__REDUX_DEVTOOLS_EXTENSION__  ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f  (deploy to firebase)
+
 )
 ReactDOM.render(
   <Provider store={store}>
